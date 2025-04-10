2b623d5533f50fd029febd27eab97579ed8c322a
Miiro <NicknameAlwaystaken@users.noreply.github.com>
2025-04-10 18:22:39 +0300

    changed neovim shell to powershell. Also removed a hotkey

--- lua/nicknamealwaystaken/set.lua
+++ lua/nicknamealwaystaken/set.lua
@@ -37,3 +37,11 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.opt.shell = "pwsh"
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -Command "
+vim.opt.shellquote = ""
+vim.opt.shellxquote = "\""
